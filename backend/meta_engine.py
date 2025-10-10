6a35aa6d44bb52573c1f8b4614f0399403a91e32
Kyle <kylemadeitso@gmail.com>
2025-10-10 16:49:13 -0400

    TEST: Deploying minimal 'Hello, Route' application

---
+++ backend/meta_engine.py
@@ -0,0 +1,11 @@
+﻿from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Route Test"}
+
+@app.post("/ingest-log")
+def ingest_log(file: UploadFile = File(...)):
+    return {"status": "success", "message": "Endpoint reached successfully", "filename": file.filename}
