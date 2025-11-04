4872367a435769ecff42892d7a41651857908e21
MISO Agent <kylemadeitso@gmail.com>
2025-11-04 01:26:41 +0000

    MISO Tier 3 (Primate): Fix TDD error

--- miso_project/workspace/stats.py
+++ miso_project/workspace/stats.py
@@ -2,4 +2,6 @@ from typing import List, Union
 
 def calculate_mean(data: List[Union[int, float]]) -> float:
     """Calculates the mean of a list of numbers."""
+    if not data:
+        return 0.0
     return sum(data) / len(data)
