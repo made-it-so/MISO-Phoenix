7fe385f71b3add0901c78cbf6b58c6b851b8b066
MISO Agent <kylemadeitso@gmail.com>
2025-11-04 01:36:40 +0000

    CHORE: Reset workspace for final Human escalation test

--- miso_project/workspace/stats.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-def calculate_mean(data: List[float]) -> float:
-    """Calculates the mean of a list of numbers."""
-    if not data:
-        return 0.0
-    return sum(data) / len(data)
